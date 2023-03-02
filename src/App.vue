83cb9361577648a8762f4ad8b0ec4b40d1ce5386
Yichen Guo <yg2258@nyu.edu>
2023-03-02 01:45:41 -0500

    sidebar svg

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
     <div id="app" class="font-sans flex min-h-screen block text-red-dark">
-        <div id="nav" class="w-64 bg-grey-lightest text-black fixed min-h-screen">
-            This is a sidebar
-        </div>
+        <sidebar></sidebar>
     <router-view/>
     </div>
 </template>
+
+<script>
+    import Sidebar from "./components/Sidebar.vue"
+
+    export default{
+        components:{
+            Sidebar,
+        }
+    }
+</script>
 
 <style src="./assets/tailwind.css"/>
