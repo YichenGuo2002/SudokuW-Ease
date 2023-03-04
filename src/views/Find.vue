d26ff725f5918afbdf103f2b56a40685c84ca2a8
Yichen Guo <yg2258@nyu.edu>
2023-03-04 16:08:14 -0500

    update discover page

--- src/views/Find.vue
+++ src/views/Find.vue
@@ -2,11 +2,13 @@
     <div class = "ml-48 mt-16 px-4 py-2 mb-4 bg-white text-black flex-1">
         <p>Pick some Sudoku puzzles! 
         <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">New York Times</button>
-        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">Los Angeles Times</button>
-        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">Washington Times</button>
-        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">The Daily Newspaper</button>
-        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">Dr Arto Inkala's world hardest puzzle</button>
-
+        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">Menneske.No 6913752 Random Sudokus</button>
+        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">Sudokuweb.Org Generator</button>
+        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">UK Sudoku Daily Competition</button>
+        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">Dr Arto Inkala's world hardest puzzle 2006</button>
+        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">Dr Arto Inkala's world hardest puzzle 2010</button>
+        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">USA Sudoku Championship 2020</button>
+        <button  class = "inline-block mx-1 my-1 px-2 py-2 rounded-lg bg-grey-lighter text-black no-underline hover:bg-grey">Dr Peter's Unsolvable Sudoku</button>
         .</p>
     </div>
 </template>
