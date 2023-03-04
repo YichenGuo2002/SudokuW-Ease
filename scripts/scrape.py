ee758a60bf7b282e249a0ec711b4c64a77fa5c6b
Yichen Guo <yg2258@nyu.edu>
2023-03-04 14:44:34 -0500

    web scrapping added

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -16,6 +16,48 @@ def scrapeNYTimes():
         'hard':ld['hard']['puzzle_data']['puzzle']
     } 
 
+# https://menneske.no/
+def scrapeMenneskeNo():
+    URL = 'https://menneske.no/sudoku/eng/random.html'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    scripts = soup.find('div', {'class':re.compile('grid')})
+    tds = scripts.find_all('td')
+    result = []
+    for td in tds:
+        if(td.text != '\xa0'):
+            result.append(int(td.text))
+        else:
+            result.append(0)
+    return result
 
+# https://www.sudokuweb.org/
+def scrapeSudokuWebOrg():
+    URL = 'https://www.sudokuweb.org/'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    scripts = soup.find('table')
+    spans = scripts.find_all('span')
+    result = []
+    for span in spans:
+        if(span['class'][0] == "sedy"):
+            result.append(int(span.text))
+        elif(span['class'][0] == 'vloz'):
+            result.append(0)
+    return result
 
-print(scrapeNYTimes())
+# https://www.sudoku.org.uk/daily.asp
+def scrapeSudokuOrgUK():
+    URL = 'https://www.sudoku.org.uk/daily.asp'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    tds = soup.find_all('td', {'class':re.compile('InnerTDone')})
+    result = []
+    for td in tds:
+        if(td.text):
+            result.append(int(td.text))
+        else:
+            result.append(0)
+    return result
+
+print(scrapePrintable())
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,118 @@
 <template>
     <div class = "ml-48 mt-16 px-4 py-2 mb-4 bg-white text-black flex-1">
-        <p>Hi from about!</p>
+        <form action="daily_submit.asp" onsubmit="javascript: return validateCompetition(document.forms.puzform);" method="post" name="puzform" id="puzform">
+	<table border="0" cellpadding="0" cellspacing="0">
+	<tbody><tr>
+		<td width="290" rowspan="3">
+		
+	<table border="0" cellpadding="0" cellspacing="0">
+        <tbody>
+            <tr>
+            <td class="InnerTDone border border-black" style="border-left: 4px solid #000000;border-top: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C11" value=""></td>
+            <td class="InnerTDone border border-black" style="border-top: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C12" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-top: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C13" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-top: 4px solid #000000;">1</td>
+            <td class="InnerTDone border border-black" style="border-top: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C15" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-top: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C16" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-top: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C17" value=""></td>
+            <td class="InnerTDone border border-black" style="border-top: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C18" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 4px solid #000000;border-top: 4px solid #000000;">6</td>
+            </tr>
+            <tr>
+            <td class="InnerTDone border border-black" style="border-left: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C21" value=""></td>
+            <td class="InnerTDone border border-black">9</td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C23" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C24" value=""></td>
+            <td class="InnerTDone border border-black"><input class="cell" type="text" size="1" maxlength="1" name="C25" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;">3</td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C27" value=""></td>
+            <td class="InnerTDone border border-black">8</td>
+            <td class="InnerTDone border border-black" style="border-right: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C29" value=""></td>
+            </tr>
+            <tr>
+            <td class="InnerTDone border border-black" style="border-left: 4px solid #000000;border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C31" value=""></td>
+            <td class="InnerTDone border border-black" style="border-bottom: 2px solid #000000;">6</td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C33" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C34" value=""></td>
+            <td class="InnerTDone border border-black" style="border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C35" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C36" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C37" value=""></td>
+            <td class="InnerTDone border border-black" style="border-bottom: 2px solid #000000;">5</td>
+            <td class="InnerTDone border border-black" style="border-right: 4px solid #000000;border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C39" value=""></td>
+            </tr>
+            <tr>
+            <td class="InnerTDone border border-black" style="border-left: 4px solid #000000;border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C41" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-top: 2px solid #000000;">9</td>
+            <td class="InnerTDone border border-black" style="border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C42" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C44" value=""></td>
+            <td class="InnerTDone border border-black" style="border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C45" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-top: 2px solid #000000;">8</td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-top: 2px solid #000000;">1</td>
+            <td class="InnerTDone border border-black" style="border-top: 2px solid #000000;">3</td>
+            <td class="InnerTDone border border-black" style="border-right: 4px solid #000000;border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C49" value=""></td>
+            </tr>
+            <tr>
+            <td class="InnerTDone border border-black" style="border-left: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C51" value=""></td>
+            <td class="InnerTDone border border-black"><input class="cell" type="text" size="1" maxlength="1" name="C52" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;">6</td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C54" value=""></td>
+            <td class="InnerTDone border border-black">4</td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C56" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;">9</td>
+            <td class="InnerTDone border border-black"><input class="cell" type="text" size="1" maxlength="1" name="C58" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C59" value=""></td>
+            </tr>
+            <tr>
+            <td class="InnerTDone border border-black" style="border-left: 4px solid #000000;border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C61" value=""></td>
+            <td class="InnerTDone border border-black" style="border-bottom: 2px solid #000000;">3</td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C63" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-bottom: 2px solid #000000;">5</td>
+            <td class="InnerTDone border border-black" style="border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C65" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-bottom: 2px solid #000000;">2</td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-bottom: 2px solid #000000;">8</td>
+            <td class="InnerTDone border border-black" style="border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C68" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 4px solid #000000;border-bottom: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C69" value=""></td>
+            </tr>
+            <tr>
+            <td class="InnerTDone border border-black" style="border-left: 4px solid #000000;border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C71" value=""></td>
+            <td class="InnerTDone border border-black" style="border-top: 2px solid #000000;">4</td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C73" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C74" value=""></td>
+            <td class="InnerTDone border border-black" style="border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C75" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C76" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C77" value=""></td>
+            <td class="InnerTDone border border-black" style="border-top: 2px solid #000000;">1</td>
+            <td class="InnerTDone border border-black" style="border-right: 4px solid #000000;border-top: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C79" value=""></td>
+            </tr>
+            <tr>
+            <td class="InnerTDone border border-black" style="border-left: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C81" value=""></td>
+            <td class="InnerTDone border border-black">2</td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C83" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;">7</td>
+            <td class="InnerTDone border border-black"><input class="cell" type="text" size="1" maxlength="1" name="C85" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C86" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C87" value=""></td>
+            <td class="InnerTDone border border-black"><input class="cell" type="text" size="1" maxlength="1" name="C88" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C89" value=""></td>
+            </tr>
+            <tr>
+            <td class="InnerTDone border border-black" style="border-left: 4px solid #000000;border-bottom: 4px solid #000000;">7</td>
+            <td class="InnerTDone border border-black" style="border-bottom: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C92" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-bottom: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C93" value=""></td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-bottom: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C94" value=""></td>
+            <td class="InnerTDone border border-black" style="border-bottom: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C95" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 2px solid #000000;border-bottom: 4px solid #000000;">9</td>
+            <td class="InnerTDone border border-black" style="border-left: 2px solid #000000;border-bottom: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C97" value=""></td>
+            <td class="InnerTDone border border-black" style="border-bottom: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C98" value=""></td>
+            <td class="InnerTDone border border-black" style="border-right: 4px solid #000000;border-bottom: 4px solid #000000;"><input class="cell" type="text" size="1" maxlength="1" name="C99" value=""></td>
+            </tr>
+
+        </tbody>
+    </table>
+    </td>
+    </tr>
+    </tbody>
+    </table>
+	</form>
     </div>
 </template>
